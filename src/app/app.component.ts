9020cf64bf6913a38702309f8b840cd300ae672a
Pawel Krol <paullkroll@icloud.com>
2026-08-31 23:35:36 +0200

    add main styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslateFallbackPipe } from '@core/pipes/translate-pipe/translate-pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TranslateFallbackPipe],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
