eac46ae63bf4cd51217ec03f0927bc134d2d8275
Pawel Krol <paullkroll@icloud.com>
2026-09-01 21:18:27 +0200

    add dashobard, login and unauth components, add basic routes, separate styles and use them in main style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
-import { Routes } from '@angular/router';
+import { Router, Routes } from '@angular/router';
+import { inject } from '@angular/core';
+import { UserDataService } from '@core/services/user-data-service/user-data';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    canActivate: [
+      () => {
+        const userDataService = inject(UserDataService);
+        const router = inject(Router);
+
+        const userData = userDataService.getUserDataFromLocalStorage();
+
+        if (userData?.user) {
+          return router.createUrlTree(['/dashboard']);
+        }
+
+        return router.createUrlTree(['/auth/login']);
+      },
+    ],
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('@features/dashboard/dashboard').then((m) => m.Dashboard),
+  },
+  {
+    path: 'auth/login',
+    loadComponent: () => import('@features/auth/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'unauthorized',
+    loadComponent: () => import('@features/unauthorized/unauthorized').then((m) => m.Unauthorized),
+  },
+];
